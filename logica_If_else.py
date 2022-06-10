3a92981d56b1d4112bc8e36bbed61047edc61264
Pedro Augusto Silva Ribeiro <pedro.outo@gmail.com>
2022-06-10 17:29:34 -0300

    segundo commit

--- logica_If_else.py
+++ logica_If_else.py
@@ -6,11 +6,30 @@ print("Bem", "Vindo!", sep=" ", end="\n")
 print("**********", end="\n")
 
 nome = str(input("Digite seu nome: "))
-numero = int(input("Digite um numero: "))
-print("O numero que", nome, "escolheu foi: ", numero)
-
+numero = int(input("Digitea sua idade: "))
 calculo = (numero % 2)
+crianca = numero < 12
+adolecente = numero < 18
+adulto = numero < 60
+ano = 2022
+anoquenasceu = (ano - numero)
+
+print("\n")
+print("***************************************************", "\n")
+print(" A sua idade é", numero, "anos e seu nome é", nome, "\n")
+
 if(calculo == 0):
-    print("O numero", numero, "é par")
+    print("", nome, "sua idade", numero, "é par", "\n")
 else:
-    print("O numero", numero, "é impar")
+    print("", nome, "sua idade", numero, "é impar", "\n")
+
+if(crianca):
+    print(" Voce e crianca")
+elif(adolecente):
+    print(" Voce e adolecente")
+elif(adulto):
+    print(" Voce e adulto")
+else:
+    print(" Voce e Idoso")
+print("\n", "Voce nasceu em", anoquenasceu, "\n")
+print("***************************************************", "\n")
