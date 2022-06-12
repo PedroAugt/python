d0883c8143c3a9f5a9d30b0d23ce4801a29bc354
PedroAugt <pedro.outo@gmail.com>
2022-06-11 21:25:24 -0300

    Aprendendo Repeticao

---
+++ logica_repeticao.py
@@ -0,0 +1,19 @@
+print("************************")
+print("\nEstruturas de repeticao\n")
+print("************************",)
+
+print("Estrutura com For e While!")
+print("Utilizando o FOR!\n")
+
+clientes = ['Pedro', 'Augusto', 'Joao', 'Lucas']
+cont = 0
+
+for x in clientes:
+    print(x)
+
+print("\nTerminando o FOR\n")
+print("Utilizando o While!\n")
+
+while cont < 10:
+    print(cont)
+    cont = cont + 1
