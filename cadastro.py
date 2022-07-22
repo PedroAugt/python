9bcd55d7b659e28c86fcf20f69c18d2111ccb111
PedroAugt <pedro.outo@gmail.com>
2022-07-22 14:59:14 -0300

    tipos de dados

---
+++ cadastro.py
@@ -0,0 +1,15 @@
+# Criando um cadastro
+# tipos de dados
+# str - textos ,'', ""
+# int - inteiro - 123, -10, 20
+# float - real / ponto flutuante - 10.10, 1.5, 0.0
+# bool - booleano / lógico - true/false
+
+# nome
+print('Pedro Augusto', type('Pedro Augusto'))
+# idade
+print('22', type(22))
+# altura
+print('1.70', type(1.70))
+# maior de idade
+print(22 > 18, type(32 > 18))
