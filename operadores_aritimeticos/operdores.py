2cbd43cd676790536c1552639457e579f25b9779
PedroAugt <pedro.outo@gmail.com>
2022-07-23 14:56:06 -0300

    commit

---
+++ operadores_aritimeticos/operdores.py
@@ -0,0 +1,18 @@
+"""
++, -, *, /, //, **, %, ()
+"""
+print('multiplicação * ', 10 * 10)
+print('adição + ', 10 + 10)
+print('subtração - ', 10 - 5)
+print('divisão / ', 10/2)
+print('potenciação **', 2 ** 10)
+print('Resto %', 10 % 3)
+print('priorizar ()', (2+2)*5, 2+2*5)
+
+"""
+comportamento diferente dos operadores
+"""
+
+print('operador de repetição *', 10 * 'A')
+print('operador de concatenar +', '10' + 'A')
+print('como' + ' ' + 'concatenar com +')
